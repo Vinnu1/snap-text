4d95f2818b2c949764eb3a329c26b829a417957e
Vinayak Sinha <vinayaksinha@zoho.com>
2018-03-06 14:57:42 +0530

    Added files

---
+++ src/js/index.js
@@ -0,0 +1,80 @@
+document.addEventListener("DOMContentLoaded",function(){
+    
+    var socket = io();
+    var Name = "Anonymous";
+    var quote;
+
+    //get DOM elements
+    var nameButton = document.getElementById("nameButton");
+    var name =  document.getElementById("name");
+    var msg = document.getElementById("msg");
+    var msgBox = document.getElementById("msgBox");
+    var send = document.getElementById("send");
+    var rq = document.getElementById("rq");
+
+    //Get quotes
+    $.getJSON("book_quotes.json",function(quote){
+      quotes = quote;     
+    });
+    
+    //Show modal onload, modal is not closed by keyboard or mouse
+    $("#nameModal").modal({backdrop: 'static', keyboard: false});
+
+    //when enter is clicked
+    nameButton.addEventListener("click",function(){
+        if(name.value != ""){
+            Name = name.value;
+        }
+        $("#nameModal").modal("hide");
+    });
+    
+    
+    
+    //send button clicked
+    send.addEventListener("click",function(){
+        let container = document.createElement("div");
+        let text = document.createElement("span");
+        container.classList.add("mt-2","text-right");
+        text.innerHTML = msg.value;
+        text.classList.add("bg-dark","text-light","px-1","py-1","rounded");
+        container.appendChild(text);
+        msgBox.appendChild(container);
+        socket.emit("message",msg.value,Name);
+        setTimeout(function(){
+            container.remove();
+        },60000);
+    });
+
+    //When RQ (random quote) button is clicked.
+    rq.addEventListener("click",function(){
+        var rand = Math.floor(Math.random()*100);
+        let rquote = quotes[rand].quote+" -"+quotes[rand].author;
+        let container = document.createElement("div");
+        let text = document.createElement("span");
+        container.classList.add("mt-2","text-right");
+        text.innerHTML = rquote;
+        text.classList.add("bg-dark","text-light","px-1","py-1","rounded");
+        container.appendChild(text);
+        msgBox.appendChild(container);
+        socket.emit("message",rquote,Name);
+        setTimeout(function(){
+            container.remove();
+        },60000); 
+    });
+
+    function recievedMessage(rmsg,rname){
+        let container = document.createElement("div");
+        let rtext = document.createElement("span");
+        rtext.innerHTML = "<strong>"+rname+"</strong>"+": "+rmsg;
+        rtext.classList.add("bg-dark","text-light","px-1","py-1","rounded");
+        container.classList.add("text-left","mt-2");
+        container.appendChild(rtext);
+        msgBox.appendChild(container);
+        setTimeout(function(){
+            container.remove();
+        },60000);
+    }
+
+    //When message arrives from server.js call recievedMessage()
+    socket.on("recieve_message",recievedMessage);
+})
